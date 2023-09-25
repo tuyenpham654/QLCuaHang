5cb9a896cd4bda6b58a76e96730e72753bd0a366
tuyen <tuyen@STEVE>
2023-09-25 11:43:55 +0700

    Add project files.

---
+++ QuanLyCuaHang/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCuaHang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Product product = new Product(10,"name","Price","weight","memory") ;
+            Console.WriteLine("+--------------------------------------+");
+            Console.WriteLine("| ID | Name | Price | Weight | Memory  |");
+            Console.WriteLine("+--------------------------------------+");
+            Console.WriteLine(product.toString());
+            Console.WriteLine("+--------------------------------------+");
+            Console.ReadKey();
+        }
+    }
+}
